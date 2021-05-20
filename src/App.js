4f17802a9297612bd7672b6878c131e6b290d270
Taylor Hughes <taylorhughes@Taylors-Air.lan>
2021-05-20 10:56:28 -0700

    added dev backend to URL

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Form from "./Form";
 
 function App() {
 
-  const url = "http://localhost:3000";
+  const url = "https://songs-backend.herokuapp.com";
 
   const [songs, setSongs] = React.useState([])
 
